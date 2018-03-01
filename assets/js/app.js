0dd5aaf8d9b6dd1bc78642e26569152b410866d7
Oscar Villalta <bagglerock@Oscars-MacBook-Pro.local>
2018-03-01 10:42:39 -0500

    Organized the mess I made last night a bit to start working on this again

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,9 @@
-//  Array of Questions with answers
+//**  The questions stored in an array of objects   **/
+
 var questions = [
   {
-    question: "If a person holds 3 widgets, How many tires can fit in a locker?",
+    question:
+      "If a person holds 3 widgets, How many tires can fit in a locker?",
     a: "Banana",
     b: "Dirt",
     c: "Umbrella",
@@ -48,52 +50,59 @@ var questions = [
   }
 ];
 
-/*for ( var i = 0; i < questions.length; i++){
-    console.log(questions[i].question);
-    console.log(questions[i].a);
-    console.log(questions[i].b);
-    console.log(questions[i].c);
-    console.log(questions[i].d);
-    console.log(questions[i].answer);
-    console.log(questions[i].link);
-}*/
+//**   Variables   **//
 
-var questionsAsked = []; //  this will be the questions that were already asked
+//  Boolean to hold the status of the game
+var gameRunning = false;
+
+//  Tallies
+var correctAnswers;
+var incorrectAnswers;
+
+//  Timer Intervals
+var intervalTimer = 30 * 1000;
+var TimeoutTimer = 5 * 1000;
+
+//  Variable to hold the interval for the question
+var showQuestion;
+
+//  Variable to hold the timeout for the answer
+var showAnswer;
+
+//  Variable to store the current question object
 var currentQuestion;
 
-// randomizes the question asked
+// Array to hold the questions that were asked
+var questionsAsked = [];
+
+//**   Functions   **//
+
+//  Function that initializes the game when starts.  Sets game to true, resets the tallies, clears out array of questions asked.
+function initialize() {
+  gameRunning = true;
+  correctAnswers = 0;
+  incorrectAnswers = 0;
+  questionsAsked = [];
+}
+
+//   Function to change the order of how the questions come out, as well as keep track of which questions were asked.
 function pickQuestion() {
   var randomNumber = Math.floor(Math.random() * questions.length);
   if (questions.length > 0) {
-    questionsAsked.push(questions[randomNumber]);
     currentQuestion = questions[randomNumber];
+    questionsAsked.push(currentQuestion);
     questions.splice(randomNumber, 1);
+    return currentQuestion; // not sure if i want it to return the object first or just leave it to update the global.
   } else {
     console.log("array is empty, no more questions to ask");
     gameRunning = false;
   }
 }
 
-$("#another-button").on("click", function() {
-  pickQuestion();
-});
+//  Randomize answers for replayability.  Put answers in an array and splice each question in an random order to display.  Somehow set the key for the correct answer
+//        - possible just set the correct answer as a long string in the object and compare each possibility with the correct answer.
 
-//  Variables
-var correctAnswers;
-var incorrectAnswers;
-var timer = 30;
-var gameRunning = false;
-var showQuestion;
-var showAnswer;
-var usedQuestions = [];
-
-//  Functions
-
-function initialize() {
-  gameRunning = true;
-  correctAnswers = 0;
-  incorrectAnswers = 0;
-}
+//**   Functions that manipulate timers   **/
 
 function startTimer() {
   //  Show the Timed Out screen after 30 seconds
@@ -102,35 +111,21 @@ function startTimer() {
 
 //  Maybe should be called show next question..  automatically chooses a next question
 function nextQuestion() {
-    if (gameRunning){   
-         clearQArea();
-        pickQuestion();
-        var questionHTML = $("<h1>");
-        questionHTML.text(currentQuestion.question);
-        $(".question-area").append(questionHTML);
-        showAnswer = setTimeout(showTimeOut, 2000);
-    } else { 
-        stopInterval();
-        displayResults();
-    }
-
-}
-
-function clearQArea () {
-    $(".question-area").empty();
-
-}
-
-function stopInterval () {
-    clearInterval(showQuestion);
-
-}
-
-function displayResults() {
+  if (gameRunning) {
     clearQArea();
-    var summary = $("<h1>");
-    summary.text("There aren't any more questions")
-    $(".question-area").append(summary);
+    pickQuestion();
+    var questionHTML = $("<h1>");
+    questionHTML.text(currentQuestion.question);
+    $(".question-area").append(questionHTML);
+    showAnswer = setTimeout(showTimeOut, 2000);
+  } else {
+    stopQuestions();
+    displayResults();
+  }
+}
+
+function stopQuestions() {
+  clearInterval(showQuestion);
 }
 
 function showTimeOut() {
@@ -141,6 +136,20 @@ function showTimeOut() {
   test.text(currentQuestion.a);
   $(".question-area").append(test);
   //  set another time out to show next question
+}
+
+//**   DOM manipulation functions   **/
+
+//  Function that clears the Question area
+function clearQArea() {
+  $(".question-area").empty();
+}
+
+function displayResults() {
+  clearQArea();
+  var summary = $("<h1>");
+  summary.text("There aren't any more questions");
+  $(".question-area").append(summary);
 }
 
 $(document).ready(function() {
@@ -180,6 +189,28 @@ After all the questions have been asked, the game stops the last timer and the s
 
 
 Add some classes like incorrect-answer and correct-answer so we can eliminate the wrong asnwers ever 10 seconds
+
+
+Assignment Directions
+
+### Option Two: Advanced Assignment (Timed Questions)
+
+![Advanced](Images/2-advanced.jpg)
+
+**[Click Here to Watch the demo](advanced-trivia-demo.mov)**.
+
+* You'll create a trivia game that shows only one question until the player answers it or their time runs out.
+
+* If the player selects the correct answer, show a screen congratulating them for choosing the right option. After a few seconds, display the next question -- do this without user input.
+
+* The scenario is similar for wrong answers and time-outs.
+
+  * If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
+  * If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
+
+* On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
+
+
 
 
 
