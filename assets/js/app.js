c56a8e19af7888e2e3440cba2f0e1b47285f8734
Oscar Villalta <bagglerock@Oscars-MacBook-Pro.local>
2018-02-28 19:17:27 -0500

    Made the initial files

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+//  Array of Questions with answers
+var questions = [];
+
+//  Variables
+var connectAnswers;
+var incorrectAnswers;
+var timer = 30;
+var gameRunning = false;
+
+//  Functions
+
+function initialize () {}
+
+function chooseQuestion () {}
+
+function startTimer () {}
+
+function showQuestion () {}
+
+function showAnswer () {}
+
+function showCongrats () {}
+
+function showWrong () {}
